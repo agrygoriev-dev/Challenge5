840f622759f413cd4353767a341aa7b41fe8118f
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2019-10-02 00:06:00 -0500

    updated product view to display category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@
 
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.includes(:category).all
     @average_price = @products ? @products.average_by_price.round(2) : 0
     @total_quantity = @products ? @products.summed_quantity : 0
   end
